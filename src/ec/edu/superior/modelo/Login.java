13148f87da5a7db0e818406076d8d5d3c9ee9e46
ASUS <ASUS@LAPTOP-FSORRA84>
2022-02-07 19:16:28 -0500

    Esta es el proyecto presentado como examen del primer parcial de la asiganatura de POO

---
+++ src/ec/edu/superior/modelo/Login.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.superior.modelo;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Login {
+
+    public Login(String user) {
+        this.user = user;
+    }
+    private String  user;
+    private String password;
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+}
